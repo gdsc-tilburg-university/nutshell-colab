d0ff19a449b8249c746f39baaba349cbfbf4e068
nojuskybartas <nojus.kybartas@gmail.com>
2022-11-14 20:35:54 +0100

    recording as a function

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ import sounddevice
 from os import getcwd
 from scipy.io.wavfile import write
 
-duration = 10
-sample_rate = 44100
 
-recording = sounddevice.rec(
-    frames=duration * sample_rate, samplerate=sample_rate, channels=1)
-print("Started Recording")
+def record_audio(duration=10, sample_rate=44100):
 
-sounddevice.wait()
-print("finished recording")
+    recording = sounddevice.rec(
+        frames=duration * sample_rate, samplerate=sample_rate, channels=1)
+    print("Started Recording")
 
-filename = f"{getcwd()}\\audio.wav"
+    sounddevice.wait()
+    print("finished recording")
 
-write(filename=filename, rate=sample_rate, data=recording)
+    filename = f"{getcwd()}\\audio.wav"
+    write(filename=filename, rate=sample_rate, data=recording)
+
+    return filename
+
+
+def transcribe_audio(filename: str):
+    return 0
+
+
+audio_file = record_audio()
+transcription = transcribe_audio(audio_file)
+
+print(transcription)
